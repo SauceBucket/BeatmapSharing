f452397f416674571b7797781209a246698cd611
Saucy <56363885+SaucySensation@users.noreply.github.com>
2019-11-13 19:42:27 -0500

    Add files via upload

--- gui.py
+++ gui.py
@@ -4,9 +4,12 @@ import sys
 from PyQt5.QtWidgets import QApplication, QWidget, QPushButton
 from PyQt5.QtGui import QIcon
 from PyQt5.QtCore import pyqtSlot
+from PyQt5.QtCore import QSize
+from PyQt5.QtGui import QImage, QPalette, QBrush
+from PyQt5.QtWidgets import *
 
 class App(QWidget):
-
+    
     def __init__(self):
         super().__init__()
         self.title = 'Beatmap Sharing'
@@ -14,8 +17,17 @@ class App(QWidget):
         self.top = 500
         self.width = 320
         self.height = 200
+        self.setStyleSheet("""
+        QWidget {
+             background-image: broccoli.png;
+            }
+        QPushButton{
+         color: #0;
+         background-color: #d6ced6;
+        }
+        """)
         self.initUI()
-    
+        
     def initUI(self):
         self.setWindowTitle(self.title)
         self.setGeometry(self.left, self.top, self.width, self.height)
@@ -23,11 +35,16 @@ class App(QWidget):
         button = QPushButton('Open Download Script', self)
         button.move(25,70)
         button.clicked.connect(self.on_click)
-
+        
+        
 
         button1 = QPushButton('Open Compilation Script', self)
         button1.move(150,70)
         button1.clicked.connect(self.on_click1)
+
+        button2 = QPushButton('Exit', self)
+        button2.move(self.width-85,self.height-30)
+        button2.clicked.connect(self.on_click2)
         
         self.show()
 
@@ -35,10 +52,15 @@ class App(QWidget):
 
     @pyqtSlot()
     def on_click(self):
+        sys.exit()
         osumap_download_script.download()
     @pyqtSlot()
     def on_click1(self):
-        osumap_compilation_script.compile()
+        sys.exit()
+        osumap_compilation_script.compile()    
+    @pyqtSlot()
+    def on_click2(self):
+        sys.exit()
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
